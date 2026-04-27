4773957dc064dd72f71a0570222136aeccdca960
tanczos-better-prodigy <murphyn@roverkids.org>
2026-04-27 14:16:55 -0400

    2.1 AnimalStampede

--- AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,6 +30,17 @@ public class PlayerController : MonoBehaviour
         {
         transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
     }
-    
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Launch a projectile from the player 
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+            }
+
+
+
+        }
+        
     }
 }
