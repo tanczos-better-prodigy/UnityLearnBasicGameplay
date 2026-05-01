fbe3dc20690e452980472cc623e306e24b150788
tanczos-better-prodigy <murphyn@roverkids.org>
2026-05-01 14:03:05 -0400

    2.4 AnimalStampede Fin

--- AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed = 15.0f;
     public float xRange = 10;
 
     public GameObject projectilePrefab;
